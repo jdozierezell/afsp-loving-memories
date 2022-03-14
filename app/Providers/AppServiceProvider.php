7f2d4d25d924f73d939507272654c5a62c4af7c6
suresh <rs.sureshkumar@yahoo.com>
2022-03-14 08:34:15 +0200

    layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		//
+		URL::forceScheme('https');
 		Schema::defaultStringLength(191);
 		if ($this->app->isLocal()) {
 			$this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
