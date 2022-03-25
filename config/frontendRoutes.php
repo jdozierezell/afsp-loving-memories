1bcbb3e736ca9d834fcfc869bb5971c513ec6e58
suresh <rs.sureshkumar@yahoo.com>
2022-03-25 14:20:31 +0200

    front end url changed

--- config/frontendRoutes.php
+++ config/frontendRoutes.php
@@ -2,14 +2,14 @@
 
 return [
 
-	'view-memory'=>'app/view-memory#{:access_token}',
+	'view-memory'=>'/view-memory#{:access_token}',
 	'edit-memory'=>'app/edit-memory#{:access_token}',
-	'admin-preview-memory'=>'/app/view-memory?admin=1#{:access_token}',
+	'admin-preview-memory'=>'/view-memory?admin=1#{:access_token}',
 	'preview-memory'=>'/app/preview-memory#{:access_token}',
-	'invite-friend-memory'=>'/app/add-friend-memory?token={:access_token}',
-	'review-friend-memory'=>'/app/review-friend-memory?token={:access_token}',
-	'review-friend-memory-pending-admin'=>'/app/review-friend-memory-admin?token={:access_token}',
-	'reset-password'=>'/app/reset-password',
-	'start-memory'=>'/app/add-memory',
-	'verify-account'=>'/app/verify-account',
+	'invite-friend-memory'=>'/add-friend-memory?token={:access_token}',
+	'review-friend-memory'=>'/review-friend-memory?token={:access_token}',
+	'review-friend-memory-pending-admin'=>'/review-friend-memory-admin?token={:access_token}',
+	'reset-password'=>'/reset-password',
+	'start-memory'=>'?add=1',
+	'verify-account'=>'/verify-account',
 ];
