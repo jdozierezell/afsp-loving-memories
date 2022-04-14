ccb8ca08b7fe55187fcce8dc0dc340ead80d19d0
suresh <rs.sureshkumar@yahoo.com>
2022-04-14 15:43:27 +0200

    added

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
 	public function handle(Request $request, Closure $next)
 	{
 		return $next($request)
-			->header('Access-Control-Allow-Origin', 'https://afsp-loving-memories.netlify.app')
+			->header('Access-Control-Allow-Origin', '*')
 			->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 			->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
 	}
